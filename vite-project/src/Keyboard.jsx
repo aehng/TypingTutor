981582c4eb9c66287481d23cc340128d80dc39a6
Elijah Kearl <elijkearl@gmail.com>
2023-11-08 12:20:50 -0700

    Shift done

--- vite-project/src/Keyboard.jsx
+++ vite-project/src/Keyboard.jsx
@@ -12,7 +12,7 @@ export function Keyboard(props) {
       <KeyboardRow shift={shift} keyValues={['`','1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=']} upperKeys={['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+']} />
       <KeyboardRow shift={shift} keyValues={['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\']} upperKeys={['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|']} />
       <KeyboardRow shift={shift} keyValues={['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'']} upperKeys={['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"']} />
-      <KeyboardRow shift={shift} keyValues={['z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/']} upperKeys={['Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?']} />
+      <KeyboardRow shift={shift} keyValues={['Shift','z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/','Shift']} upperKeys={['Shift','Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?','Shift']} />
       <div className="keyboard-key space-bar"> </div>
     </div>
   )
--- vite-project/src/KeyboardRow.jsx
+++ vite-project/src/KeyboardRow.jsx
@@ -8,21 +8,26 @@ export function KeyboardRow(props) {
   } = props;
 
   const [row, setRow] = useState([]);
-//ChatGPT helped me with this mapping function
+
+  //ChatGPT helped me remember that I am an idiot. (I couldn't remember how to make a variable <insert facepalm emoji>)
   useEffect(() => {
+    //I made this if statement not use duplicate code
+    let keys = keyValues
     if (shift) {
-      setRow(upperKeys.map((k, index) => (
-        <div key={index} className="keyboard-key">
-          {k}
-        </div>
-      )))
-    } else {
-      setRow(keyValues.map((k, index) => (
-        <div key={index} className="keyboard-key">
-          {k}
-        </div>
-      )))
+      keys = upperKeys
     }
+    let old = []
+    keys.forEach((k, index) => {
+      if (k === "Shift") {
+        old = [...old, <div key={index} className="keyboard-key shift">{k}</div>]
+      } else {
+        old = [...old, <div key={index} className="keyboard-key">
+          {k}
+        </div>]
+      }
+    })
+    setRow(old)
+
   }, [shift]);
 
 
